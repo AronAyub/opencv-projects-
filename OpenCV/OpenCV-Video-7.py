2afd9d11943d82b1da27a3797596148c92626b7f
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-02-01 16:10:45 +0300

    open camera and flip the video accordingly

--- OpenCV/OpenCV-Video-7.py
+++ OpenCV/OpenCV-Video-7.py
@@ -1,10 +1,17 @@
+# lauches the laptop cameara
+
 import cv2
 import numpy as np
-
 cam = cv2.VideoCapture(0)
 while True:
     myret, CapFrame =cam.read()
-    cv2.imshow('CapFrame',CapFrame)
+
+    mirrored_frame = cv2.flip(CapFrame, 1)
+
+    # Display the mirrored frame
+    cv2.imshow('Mirrored Camera Feed', mirrored_frame)
+
+    #cv2.imshow('CapFrame',CapFrame)
     if cv2.waitKey(10) & 0xFF==ord('c'):
         cv2.imwrite('CapFrame.jpg', CapFrame)
         break
